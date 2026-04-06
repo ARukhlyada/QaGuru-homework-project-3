ed4279cebd7c8eaa2c960dd12d5a95a2897e0d47
Alexandr Ruhlyada <evildeadii@mail.ru>
2026-04-06 17:05:35 +0300

    added new tests

--- src/test/java/MyTests.java
+++ src/test/java/MyTests.java
@@ -26,182 +26,135 @@ public class MyTests {
         closeWebDriver();
         System.gc();
         System.out.println("Браузер закрыт");
-
     }
 
     @Test
     void fillFormWithPopupHandling() {
-        open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
-
+        open("/automation-practice-form.html");
         // Закрываем рекламный попап
         closeAdPopup();
-
         // Находим элемент по id и вводим текст
         $("#firstName").setValue("Вася");
         $("#lastName").setValue("Васин");
         $("#userEmail").setValue("ivan@example.com");
-        //проверка
-        $("#firstName").shouldHave(value("Вася"));
-        $("#lastName").shouldHave(value("Васин"));
-        $("#userEmail").shouldHave(value("ivan@example.com"));
-
         // Выбор пола
-        $("#gender-radio-1").parent().click();
-        //проверка
-        $("#gender-radio-1").shouldBe(checked);
-
+        $("#genterWrapper").$(byText("Male")).click();
         // Вводим номер телефона
         $("#userNumber").setValue("1234567890");
-        //проверка
-        $("#userNumber").shouldHave(value("1234567890"));
-
         // Вводим дату рождения
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("July");
         $(".react-datepicker__year-select").selectOption("1995");
         $(".react-datepicker__day--015").click();
-        //проверка
-        $("#dateOfBirthInput").shouldHave(value("15 Jul 1995"));
-
         // Выбираем предмет
         $("#subjectsInput").setValue("Biology").pressEnter();
-        //проверка
-        $(byText("Biology")).shouldBe(visible);
-
         // Выбор хобби
-        $("#hobbies-checkbox-1").parent().click();
-        //проверка
-        $("#hobbies-checkbox-1").shouldBe(checked);
-
+        $("#hobbiesWrapper").$(byText("Sports")).click();
         //прокручиваем страницу вниз
         executeJavaScript("window.scrollBy(0, 500)");
-
         // Загружаем картинку
-        $("#uploadPicture").uploadFile(new File("src/test/resources/images.jpg"));
-        System.out.println("Файл images.jpg загружен");
-        //проверка - не прокатило, там один элемент и я так и не придумал как из него вытащить текст, просто написал что фал загружен
-        //$(".modal-body").shouldHave(text("images.jpg"));
-
+        $("#uploadPicture").uploadFromClasspath("images.jpg");
         // Ввод адреса
         $("#currentAddress").setValue("Deathclaw st. 69");
-        //проверка
-        $("#currentAddress").shouldHave(value("Deathclaw st. 69"));
-
-
         // Выбор штата
         $("#state").click();
-        $(byText("NCR")).click();
-
+        $("#state").parent().$(byText("NCR")).click();
         // Выбор города
         $("#city").click();
-        $(byText("Delhi")).click();
-        //проверки
-        // Проверка выбранного значения в выпадающем списке
-        $("#state").shouldHave(text("NCR"));
-        $("#city").shouldHave(text("Delhi"));
+        $("#city").parent().$(byText("Delhi")).click();
+        //жмем ввод
+        $("#submit").scrollTo().click();
+        //проверки:
+        // Проверяем, что модалка открылась
+        $("#resultModal").shouldBe(visible);
+        // Проверяем каждый элемент
+        $("#resultModal").shouldHave(text("Вася Васин"));
+        $("#resultModal").shouldHave(text("ivan@example.com"));
+        $("#resultModal").shouldHave(text("Male"));
+        $("#resultModal").shouldHave(text("1234567890"));
+        $("#resultModal").shouldHave(text("1995-07-15"));
+        $("#resultModal").shouldHave(text("Biology"));
+        $("#resultModal").shouldHave(text("Sports"));
+        $("#resultModal").shouldHave(text("images.jpg"));
+        $("#resultModal").shouldHave(text("Deathclaw st. 69"));
+        $("#resultModal").shouldHave(text("NCR Delhi"));
     }
 
     @Test
-    void RequiredFieldsOnly() {
-        open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
-
+    void requiredFieldsOnly() {
+        open("/automation-practice-form.html");
         // Закрываем рекламный попап
         closeAdPopup();
-
         // заполняем имя и фамилию
         $("#firstName").setValue("Вася");
         $("#lastName").setValue("Васин");
-
         // Выбор пола
-        $("#gender-radio-1").parent().click();
-
+        $("#genterWrapper").$(byText("Male")).click();
         // Вводим номер телефона
         $("#userNumber").setValue("1234567890");
-
         //жмем ввод
         $("#submit").scrollTo().click();
-
         // Проверяем, что модалка открылась
         $("#resultModal").shouldBe(visible);
         // Проверяем каждый элемент
         $("#resultModal").shouldHave(text("Вася Васин"));
         $("#resultModal").shouldHave(text("Male"));
         $("#resultModal").shouldHave(text("1234567890"));
-
         // Закрываем модалку
         $("#closeModal").click();
     }
 
     @Test
-    void NegativeTest1 () {
-        open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
-
+    void negativeTest1 () {
+        open("/automation-practice-form.html");
         // Закрываем рекламный попап
         closeAdPopup();
-
         // заполняем ТОЛЬКО имя и фамилию
         $("#firstName").setValue("Вася");
         $("#lastName").setValue("Васин");
-
         // жмем ввод
         $("#submit").scrollTo().click();
-
         // Модалка не открыласт
         $("#resultModal").shouldNotBe(visible);
-
         //проверка на наличие ошибки
         $("#formError").shouldHave(cssClass("error"));
     }
 
     @Test
-    void NegativeTest2 () {
-        open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
-
+    void negativeTest2 () {
+        open("/automation-practice-form.html");
         // Закрываем рекламный попап
         closeAdPopup();
-
         // pfgjkyztv bvz b afvbkb.т
         $("#firstName").setValue("Вася");
         $("#lastName").setValue("Васин");
-
         // Выбор пола
         $("#gender-radio-1").parent().click();
-
         // Вводим короткий номер телефона
         $("#userNumber").setValue("123890");
-
         // жмем ввод
         $("#submit").scrollTo().click();
-
         // Модалка не открыласт
         $("#resultModal").shouldNotBe(visible);
-
         //проверка на наличие ошибки
         $("#formError").shouldHave(cssClass("error"));
         $("#formError").shouldHave(text("Please fill required fields and enter a valid 10-digit mobile number"));
         }
 
         @Test
-        void NegativeTest3 () { //в системе баг, если не заполнить какое-либо поле, то ошибка будет про некорректный номер, я бы сделал на это автотест, если бы знал какой текст должен был бы быть
-            open("https://qa-guru.github.io/one-page-form/automation-practice-form.html");
-
+        void negativeTest3 () { //в системе баг, если не заполнить какое-либо поле, то ошибка будет про некорректный номер, я бы сделал на это автотест, если бы знал какой текст должен был бы быть
+            open("/automation-practice-form.html");
             // Закрываем рекламный попап
             closeAdPopup();
-
             // Заполянем ТОЛЬКО иммя
             $("#firstName").setValue("Вася");
-
             // Выбор пола
             $("#gender-radio-1").parent().click();
-
             // Вводим номер телефона
             $("#userNumber").setValue("1234567890");
-
             $("#submit").scrollTo().click();
-
             // Модалка не появилась
             $("#resultModal").shouldNotBe(visible);
-
             //проверка на наличие ошибки
             $("#formError").shouldHave(cssClass("error"));
         }
